26042451a36c0b99f4b82096fed936d87d6c7400
shiva tej soma <shivatej82@gmail.com>
2023-12-21 02:18:56 -0500

    more details and better description

--- main.py
+++ main.py
@@ -5,21 +5,35 @@ import requests
 import bs4
 import time
 
+last_date = None
+
 # Function to check the progress and send a Pushbullet notification if width > 90
 def check_progress_and_notify(tracking_number):
+    global last_date
+
     url_to_track = f'https://www.loomisexpress.com/loomship/Track/TrackResults?t=WAYBILL&s={tracking_number}#{tracking_number}'
     res = requests.get(url_to_track)
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
 
     # Find class progress-bar and check the width
     progress_bar = soup.find('div', class_='progress-bar')
+
+    # Find the last 'tracking-detail' row
+    last_tracking_detail = soup.find_all('div', class_='tracking-detail')[1]
     width = int(progress_bar['style'].split(';')[0].split(':')[1][:-1].strip())
 
-    print(f"Progress Bar Width: {width}%")
+    # Extract the date, status, and location from the last 'tracking-detail'
+    details = [div.get_text(strip=True) for div in last_tracking_detail.find_all('div', class_='col-md-4')]
 
-    if width > 90:
+    # details now contains the date, status, and location
+    date, status, location = details
+
+    if last_date != date:
+        last_date = date
+        print(f"Date: {date}\nStatus: {status}\nLocation: {location}\nProgress Bar Width: {width}%")
         pb = Pushbullet(keys.PUSHBULLET_ACCESS_TOKEN)
-        pb.push_note("Progress Alert", f"Progress Bar Width: {width}%")
+        pb.push_note("Progress Alert", f"Date: {date}\nStatus: {status}\nLocation: {location}\nProgress Bar Width: {width}%\nLink: {url_to_track}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
